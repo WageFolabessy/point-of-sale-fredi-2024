64c2fcf29bfe56a83242e632d925c87f62a0061d
WageFolabessy <mbappepsg336@gmail.com>
2024-07-08 01:21:08 +0700

    memperbaiki kalender, kalender bisa menambah dan menghapus event

--- resources/views/pages/kalender.blade.php
+++ resources/views/pages/kalender.blade.php
@@ -26,21 +26,14 @@
                     <div class="sticky-top mb-3">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Draggable Events</h4>
+                                <h4 class="card-title">Events</h4>
                             </div>
                             <div class="card-body">
                                 <!-- the events -->
                                 <div id="external-events">
-                                    <div class="external-event bg-success">Lunch</div>
-                                    <div class="external-event bg-warning">Go home</div>
-                                    <div class="external-event bg-info">Do homework</div>
-                                    <div class="external-event bg-primary">
-                                        Work on UI design
-                                    </div>
-                                    <div class="external-event bg-danger">Sleep tight</div>
-                                    <div class="checkbox">
+                                    <div class="checkbox d-none">
                                         <label for="drop-remove">
-                                            <input type="checkbox" id="drop-remove" />
+                                            <input type="checkbox" id="drop-remove"/>
                                             remove after drop
                                         </label>
                                     </div>
@@ -113,175 +106,183 @@
     <script src="{{ asset('assets/plugins/moment/moment.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/fullcalendar/main.js') }}"></script>
     <script>
-        $(function () {
-        /* initialize the external events
-     -----------------------------------------------------------------*/
-        function ini_events(ele) {
-          ele.each(function () {
-            // create an Event Object (https://fullcalendar.io/docs/event-object)
-            // it doesn't need to have a start or end
-            var eventObject = {
-              title: $.trim($(this).text()), // use the element's text as the event title
-            };
+        $(function() {
+            function ini_events(ele) {
+                ele.each(function() {
+                    var eventObject = {
+                        title: $.trim($(this).text()),
+                        backgroundColor: $(this).css("background-color"),
+                        borderColor: $(this).css("border-color"),
+                        textColor: $(this).css("color")
+                    };
+                    $(this).data('eventObject', eventObject);
+                    $(this).draggable({
+                        zIndex: 1070,
+                        revert: true,
+                        revertDuration: 0,
+                        stop: function(event, ui) {
+                            var x = ui.offset.left;
+                            var y = ui.offset.top;
+                            var containerOffset = $('#external-events').offset();
+                            var containerWidth = $('#external-events').outerWidth();
+                            var containerHeight = $('#external-events').outerHeight();
+                            var calendarOffset = $('#calendar').offset();
+                            var calendarWidth = $('#calendar').outerWidth();
+                            var calendarHeight = $('#calendar').outerHeight();
 
-            // store the Event Object in the DOM element so we can get to it later
-            $(this).data("eventObject", eventObject);
-
-            // make the event draggable using jQuery UI
-            $(this).draggable({
-              zIndex: 1070,
-              revert: true, // will cause the event to go back to its
-              revertDuration: 0, //  original position after the drag
-            });
-          });
-        }
-
-        ini_events($("#external-events div.external-event"));
-
-        /* initialize the calendar
-     -----------------------------------------------------------------*/
-        //Date for the calendar events (dummy data)
-        var date = new Date();
-        var d = date.getDate(),
-          m = date.getMonth(),
-          y = date.getFullYear();
-
-        var Calendar = FullCalendar.Calendar;
-        var Draggable = FullCalendar.Draggable;
-
-        var containerEl = document.getElementById("external-events");
-        var checkbox = document.getElementById("drop-remove");
-        var calendarEl = document.getElementById("calendar");
-
-        // initialize the external events
-        // -----------------------------------------------------------------
-
-        new Draggable(containerEl, {
-          itemSelector: ".external-event",
-          eventData: function (eventEl) {
-            return {
-              title: eventEl.innerText,
-              backgroundColor: window
-                .getComputedStyle(eventEl, null)
-                .getPropertyValue("background-color"),
-              borderColor: window
-                .getComputedStyle(eventEl, null)
-                .getPropertyValue("background-color"),
-              textColor: window
-                .getComputedStyle(eventEl, null)
-                .getPropertyValue("color"),
-            };
-          },
-        });
-
-        var calendar = new Calendar(calendarEl, {
-          headerToolbar: {
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          },
-          themeSystem: "bootstrap",
-          //Random default events
-          events: [
-            {
-              title: "All Day Event",
-              start: new Date(y, m, 1),
-              backgroundColor: "#f56954", //red
-              borderColor: "#f56954", //red
-              allDay: true,
-            },
-            {
-              title: "Long Event",
-              start: new Date(y, m, d - 5),
-              end: new Date(y, m, d - 2),
-              backgroundColor: "#f39c12", //yellow
-              borderColor: "#f39c12", //yellow
-            },
-            {
-              title: "Meeting",
-              start: new Date(y, m, d, 10, 30),
-              allDay: false,
-              backgroundColor: "#0073b7", //Blue
-              borderColor: "#0073b7", //Blue
-            },
-            {
-              title: "Lunch",
-              start: new Date(y, m, d, 12, 0),
-              end: new Date(y, m, d, 14, 0),
-              allDay: false,
-              backgroundColor: "#00c0ef", //Info (aqua)
-              borderColor: "#00c0ef", //Info (aqua)
-            },
-            {
-              title: "Birthday Party",
-              start: new Date(y, m, d + 1, 19, 0),
-              end: new Date(y, m, d + 1, 22, 30),
-              allDay: false,
-              backgroundColor: "#00a65a", //Success (green)
-              borderColor: "#00a65a", //Success (green)
-            },
-            {
-              title: "Click for Google",
-              start: new Date(y, m, 28),
-              end: new Date(y, m, 29),
-              url: "https://www.google.com/",
-              backgroundColor: "#3c8dbc", //Primary (light-blue)
-              borderColor: "#3c8dbc", //Primary (light-blue)
-            },
-          ],
-          editable: true,
-          droppable: true, // this allows things to be dropped onto the calendar !!!
-          drop: function (info) {
-            // is the "remove after drop" checkbox checked?
-            if (checkbox.checked) {
-              // if so, remove the element from the "Draggable Events" list
-              info.draggedEl.parentNode.removeChild(info.draggedEl);
+                            // Check if dragged out of external-events container but not into the calendar
+                            if ((x < containerOffset.left || x > (containerOffset.left + containerWidth) ||
+                                 y < containerOffset.top || y > (containerOffset.top + containerHeight)) &&
+                                !(x >= calendarOffset.left && x <= (calendarOffset.left + calendarWidth) &&
+                                  y >= calendarOffset.top && y <= (calendarOffset.top + calendarHeight))) {
+                                if (confirm('Are you sure you want to remove this external event?')) {
+                                    $(this).remove();
+                                    saveExternalEvents();
+                                }
+                            }
+                        }
+                    });
+                });
             }
-          },
+
+            ini_events($('#external-events div.external-event'));
+
+            var Calendar = FullCalendar.Calendar;
+            var Draggable = FullCalendar.Draggable;
+
+            var containerEl = document.getElementById('external-events');
+            var checkbox = document.getElementById('drop-remove');
+            var calendarEl = document.getElementById('calendar');
+
+            new Draggable(containerEl, {
+                itemSelector: '.external-event',
+                eventData: function(eventEl) {
+                    return {
+                        title: eventEl.innerText,
+                        backgroundColor: window.getComputedStyle(eventEl, null).getPropertyValue('background-color'),
+                        borderColor: window.getComputedStyle(eventEl, null).getPropertyValue('background-color'),
+                        textColor: window.getComputedStyle(eventEl, null).getPropertyValue('color'),
+                    };
+                },
+            });
+
+            var calendar = new Calendar(calendarEl, {
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay',
+                },
+                themeSystem: 'bootstrap',
+                editable: true,
+                droppable: true,
+                events: JSON.parse(localStorage.getItem('calendarEvents')) || [],
+                drop: function(info) {
+                    if (checkbox.checked) {
+                        info.draggedEl.parentNode.removeChild(info.draggedEl);
+                        saveExternalEvents();
+                    }
+                    saveEvents();
+                },
+                eventReceive: function(info) {
+                    if (checkbox.checked) {
+                        info.draggedEl.parentNode.removeChild(info.draggedEl);
+                        saveExternalEvents();
+                    }
+                    saveEvents();
+                },
+                eventDragStop: function(info) {
+                    var x = info.jsEvent.pageX;
+                    var y = info.jsEvent.pageY;
+                    var calendarRect = calendarEl.getBoundingClientRect();
+
+                    if (x < calendarRect.left || x > calendarRect.right || y < calendarRect.top || y > calendarRect.bottom) {
+                        if (confirm('Are you sure you want to remove this event?')) {
+                            info.event.remove();
+                            saveEvents();
+                        }
+                    }
+                },
+                eventAdd: saveEvents,
+                eventChange: saveEvents,
+                eventRemove: saveEvents,
+            });
+
+            calendar.render();
+
+            function saveEvents() {
+                var events = calendar.getEvents().map(event => ({
+                    id: event.id,
+                    title: event.title,
+                    start: event.start,
+                    end: event.end,
+                    backgroundColor: event.backgroundColor,
+                    borderColor: event.borderColor,
+                    textColor: event.textColor,
+                }));
+                localStorage.setItem('calendarEvents', JSON.stringify(events));
+            }
+
+            function loadExternalEvents() {
+                var storedEvents = JSON.parse(localStorage.getItem('externalEvents')) || [];
+                storedEvents.forEach(event => {
+                    var eventEl = $("<div />");
+                    eventEl.css({
+                        "background-color": event.backgroundColor,
+                        "border-color": event.borderColor,
+                        color: event.textColor,
+                    }).addClass("external-event");
+                    eventEl.text(event.title);
+                    $("#external-events").prepend(eventEl);
+                    ini_events(eventEl);
+                });
+            }
+
+            function saveExternalEvents() {
+                var externalEvents = [];
+                $("#external-events .external-event").each(function() {
+                    var eventEl = $(this);
+                    externalEvents.push({
+                        title: eventEl.text(),
+                        backgroundColor: eventEl.css("background-color"),
+                        borderColor: eventEl.css("border-color"),
+                        textColor: eventEl.css("color")
+                    });
+                });
+                localStorage.setItem('externalEvents', JSON.stringify(externalEvents));
+            }
+
+            loadExternalEvents();
+
+            /* ADDING EVENTS */
+            var currColor = "#3c8dbc"; // Red by default
+            $("#color-chooser > li > a").click(function(e) {
+                e.preventDefault();
+                currColor = $(this).css("color");
+                $("#add-new-event").css({
+                    "background-color": currColor,
+                    "border-color": currColor,
+                });
+            });
+            $("#add-new-event").click(function(e) {
+                e.preventDefault();
+                var val = $("#new-event").val();
+                if (val.length == 0) {
+                    return;
+                }
+                var event = $("<div />");
+                event.css({
+                    "background-color": currColor,
+                    "border-color": currColor,
+                    color: "#fff",
+                }).addClass("external-event");
+                event.text(val);
+                $("#external-events").prepend(event);
+                ini_events(event);
+                saveExternalEvents();
+                $("#new-event").val("");
+            });
+
         });
-
-        calendar.render();
-        // $('#calendar').fullCalendar()
-
-        /* ADDING EVENTS */
-        var currColor = "#3c8dbc"; //Red by default
-        // Color chooser button
-        $("#color-chooser > li > a").click(function (e) {
-          e.preventDefault();
-          // Save color
-          currColor = $(this).css("color");
-          // Add color effect to button
-          $("#add-new-event").css({
-            "background-color": currColor,
-            "border-color": currColor,
-          });
-        });
-        $("#add-new-event").click(function (e) {
-          e.preventDefault();
-          // Get value and make sure it is not null
-          var val = $("#new-event").val();
-          if (val.length == 0) {
-            return;
-          }
-
-          // Create events
-          var event = $("<div />");
-          event
-            .css({
-              "background-color": currColor,
-              "border-color": currColor,
-              color: "#fff",
-            })
-            .addClass("external-event");
-          event.text(val);
-          $("#external-events").prepend(event);
-
-          // Add draggable funtionality
-          ini_events(event);
-
-          // Remove event from text input
-          $("#new-event").val("");
-        });
-      });
     </script>
+
 @endsection
