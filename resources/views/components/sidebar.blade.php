545e3659b469f77f5c1c1295a6441769be4f35cf
WageFolabessy <mbappepsg336@gmail.com>
2024-08-16 23:03:37 +0700

    mempebaharui nama toko di sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,7 +4,7 @@
     <a href="/" class="brand-link">
         <img src="{{ asset('assets/plugins/adminlte/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: 0.8" />
-        <span class="brand-text font-weight-light">Henk</span>
+        <span class="brand-text font-weight-light">Henx's</span>
     </a>
 
     <!-- Sidebar -->
